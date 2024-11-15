7c3c26279b37e4da6fbacd6913f4d23ab2d340a1
Azhenpia <291753162@qq.com>
2024-11-15 14:14:57 -0600

    Lecture 2 assignment

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -14,7 +14,18 @@ if (x !== 3) {
 if (x === 3) {
   console.log(y);
 }
+// var y;
+// var x;
+// variable hoisting to the top -> now is global
 
+// x !== 3 is true, then jump into first if-statement
+// the value of y is firstly assigned in line 8
+// => line 7: print undefined
+// In line 10, x is reassigned as 3
+// => line 12: print 5, (assigned in line 8)
+
+// x === 3 is true, then jump into second if-statement
+// => line 15: print 5
 
 // 2.
 var x = 3;
@@ -23,4 +34,7 @@ if (x === 3) {
   console.log(x);
 }
 console.log(x);
-
+// 2
+// 2
+// var is for function-scoped defining variable
+// x is not in function, so now x is global variable
--- homeworks/lecture2/hw6.js
+++ homeworks/lecture2/hw6.js
@@ -2,15 +2,30 @@
 
 // 1. Write a function that returns the largest element in a list.
 function largestElement(list) {
-    // implement your code here
+  if (list.length === 0) throw new Error("list is empty.");
+
+  let largest = list[0];
+  for (let item of list) {
+    if (item > largest) largest = item;
+  }
+  return largest;
 }
 
 // 2. Write function that reverses a list, preferably in place.
 function reverseList(list) {
-    // implement your code here
+  for (let i = 0, j = list.length - 1; i < j; ++i, --j) {
+    let tmp = list[i];
+    list[i] = list[j];
+    list[j] = tmp;
+  }
 }
 
 // 3. Write a function that checks whether an element occurs at least twice in a list.
 function checkTwice(list, element) {
-    // implement your code here
+  let cnt = 0;
+  for (let item of list) {
+    if (item === element) cnt++;
+    if (cnt === 2) return true;
+  }
+  return false;
 }
