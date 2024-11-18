7baf020cb139b00991b238da525e599f9be57569
Merlin <linmei.merlin@gmail.com>
2024-11-18 10:16:17 -0500

    homework2

--- homeworks/lecture2/hw6.js
+++ homeworks/lecture2/hw6.js
@@ -2,15 +2,35 @@
 
 // 1. Write a function that returns the largest element in a list.
 function largestElement(list) {
-    // implement your code here
+  // implement your code here
+  let num = 0;
+  list.forEach((element) => {
+    if (element > num) {
+      num = element;
+    }
+  });
+  return num;
 }
 
 // 2. Write function that reverses a list, preferably in place.
 function reverseList(list) {
-    // implement your code here
+  // implement your code here
+  let newList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    const element = list[i];
+    newList.push(element);
+  }
+  return newList;
 }
 
 // 3. Write a function that checks whether an element occurs at least twice in a list.
 function checkTwice(list, element) {
-    // implement your code here
+  // implement your code here
+  let num = 0;
+  list.forEach((x) => {
+    if (x === element) {
+      num += 1;
+    }
+  });
+  return num >= 2;
 }
