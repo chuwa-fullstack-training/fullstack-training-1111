71cd930032bc30e6d9ba3344c287340d5c49724d
Jianganchen <jianganchen@ufl.edu>
2024-11-27 14:40:29 -0500

    lecture5 hw2,4 finished

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,34 @@
  */
 function reverseWords(str) {
   // your code here
+  let stack = [];
+  let word = '';
+  let len = str.length;
+  
+  for (let i = 0; i < len; i++) {
+    if (str[i] !== ' ') {
+      word = word + str[i]
+    }else {
+      stack.push(word)
+      word = ''
+    }
+    if (i === len - 1) {
+      stack.push(word)
+    }
+  }
+
+  let ans = '';
+  let lenStack = stack.length;
+  for (let i = lenStack - 1; i >= 0; i--) {
+    ans += stack[i];
+    ans += " ";
+    stack.pop();
+  }
+
+  return ans;
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
-reverseWords(input);
+const input2 = 'Dancing in the rain'.split(''); // ['D', 'a', 'n', 'c', 'i', 'n', 'g', ' ', 'i', 'n', ' ', 't', 'h', 'e', ' ', 'r', 'a', 'i', 'n']
+console.log(reverseWords(input));
+console.log(reverseWords(input2));
--- homeworks/lecture5/hw4.js
+++ homeworks/lecture5/hw4.js
@@ -12,8 +12,13 @@ Promise.resolve(1)
   .then(res => {
     console.log(res);
   });
+// Output:
+// 1
+// 2
+// The first 1 output is from the console.log. The second 2 output is the return value.
+// 3 wouldn't be in the output because there's no error triggered in the execution. Also the '.then' method won't be triggered.
 
-// // 2
+// 2
 Promise.reject(1)
   .then(res => {
     console.log(res);
@@ -26,6 +31,12 @@ Promise.reject(1)
   .then(res => {
     console.log(res);
   });
+// Output:
+// 1
+// 3
+// This time the first '.then' method wouldn't be triggered because it only rejects 1.
+// The '.catch' method is triggered, outputing 1 and 3.
+// The second '.then' is invoked but 'res' has no value therefore nothing is output.
 
 //3
 function runAsync(x) {
@@ -45,3 +56,7 @@ function runReject(x) {
 Promise.all([runAsync(1), runReject(4), runAsync(3), runReject(2)])
   .then(res => console.log(res))
   .catch(err => console.log(err));
+// Output:
+// Error: 2
+// Since here it's using Promise.all, only '.catch' method would be executed. All the resolved values wouldn't be output.
+// Here '.catch' only catches the first returned value. Value 2 is returned earlier than value 4 so it outputs 'Error: 2'.
