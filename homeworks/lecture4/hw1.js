b3f6c6b36fe5ad22b114d9cdb833d44ceb10b2c6
JujieX <jx2320@columbia.edu>
2024-12-05 14:17:15 -0800

    feat: hw for lecture4

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -5,4 +5,26 @@
 // <html><head><title>My Title</title></head></head></html> - false
 // <html><head><title>My Title</title></head></html - true
 
-function checkValidHTML(html) {}
+function checkValidHTML(html) {
+  // stack
+  // use regular express find the <> and </>
+
+  const tagRegex = /<\/?([a-zA-Z]+)>/g;
+
+  let stack = [];
+  let match = tagRegex.exec(html);
+  while (match !== null);
+  {
+    const [fullTag, tagName] = match;
+    if (fullTag.startsWith("</")) {
+      if (stack.length === 0 || stack.pop() !== tagName) {
+        return false;
+      }
+    } else {
+      stack.push(tagName);
+      match = tagRegex.exec(html);
+    }
+  }
+
+  return stack.length === 0;
+}
--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -1,6 +1,6 @@
 /** implement Singleton pattern in both ES5 and ES6
  * https://en.wikipedia.org/wiki/Singleton_pattern
- * 
+ *
  * Example:
  * const instance1 = new Singleton();
  * const instance2 = new Singleton();
@@ -8,3 +8,32 @@
  */
 
 // your code here
+// es5
+function Singleton() {
+  if (Singleton.instance) {
+    return Singleton.instance;
+  }
+  Singleton.instance = this;
+  return this;
+}
+// es6
+class Singleton {
+  constructor() {
+    if (Singleton.instance) {
+      return Singleton.instance;
+    }
+    Singleton.instance = this;
+  }
+}
+
+// this is the common way when I write singleton
+class Singleton {
+  static _ins;
+
+  static get ins() {
+    if (!this._ins) {
+      this._ins = new Singleton();
+    }
+    return this._ins;
+  }
+}
