eb66a28852c977edb04e1e6bf11b9793e2491cab
Nan Yang <nanyang.sde@gmail.com>
2024-12-16 16:19:59 -0500

    APIs working

--- homeworks/lecture10/demo/index.js
+++ homeworks/lecture10/demo/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./db')
+
+const Todo = require('./models/todo')
 
 const app = express();
+connectDB()
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -9,27 +13,51 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-const todos = [
-  { id: 1, todo: 'first thing', done: true },
-  { id: 2, todo: 'second thing', done: false },
-  { id: 3, todo: 'third thing', done: false }
-];
+// const todos = [
+//   { id: 1, todo: 'first thing', done: true },
+//   { id: 2, todo: 'second thing', done: false },
+//   { id: 3, todo: 'third thing', done: false }
+// ];
 
-app.get('/', (req, res) => {
-  res.render('index', { todos });
+app.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.find()
+    res.render('index', { todos });
+  } catch(err){
+    console.log(err.message)
+    res.status(500).json({ error: err.message})
+  }
 });
 
-app.post('/api/todos', (req, res) => {
-  const todo = req.body.todo;
-  todos.push({ id: todos.length + 1, todo, done: false });
-  res.json(todos);
+app.post('/api/todos', async (req, res) => {
+  try {
+    await new Todo(req.body).save()
+    res.status(200).json({ message: "New todo item added" })
+
+  } catch(err) {
+    console.log(err.message)
+    res.status(500).json({ error: err.message})
+  }
 });
 
-app.put('/api/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const todo = todos.find(t => t.id === id);
-  todo.done = !todo.done;
-  res.json(todo);
+app.put('/api/todos/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const todo = await Todo.findById({ _id: id })
+    todo.isDone = !todo.isDone
+    const newTodo = await todo.save()
+    res.status(200).json({ todo: newTodo })
+
+  } catch(err){
+    console.log(err.message)
+    res.status(500).json({ error: err.message})
+  }
+
+  // const id = parseInt(req.params.id, 10);
+  // const todo = todos.find(t => t.id === id);
+  // todo.done = !todo.done;
+  // res.json(todo);
 });
 
 app.listen(3000, () => {
