bac7c07c78b2194f29c83ddb58c589b77c635ba5
Arvin-tc <arvinnliutc@gmail.com>
2024-12-19 16:54:15 -0800

    update

--- homeworks/lecture10/todo/server.js
+++ homeworks/lecture10/todo/server.js
@@ -7,16 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-
-const dbURI = 'mongodb+srv://arvinnliutc:liu@123.com@firsttry.guiff.mongodb.net/?retryWrites=true&w=majority&appName=FirstTry';
-
-mongoose.connect(dbURI, {
+mongoose.connect('mongodb://localhost/todo-list', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-.then(() => {
-  console.log('MongoDB connected successfully');
-})
-.catch((err) => {
-  console.error('MongoDB connection error:', err);
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.log('MongoDB connection error:', err);
+});
+
+const todoRoutes = require('./routes/todoRoutes');
+app.use('/', todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
